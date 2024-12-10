f1c479358e8967ded5ed05373809f3da17afd407
Prawirdani <prawirdani@gmail.com>
2024-12-10 21:55:29 +0700

    implement vendors_controller

--- lib/route/api_route.dart
+++ lib/route/api_route.dart
@@ -1,3 +1,4 @@
+import 'package:paml_rest_api/app/http/controllers/vendors_controller.dart';
 import 'package:vania/vania.dart';
 
 class ApiRoute implements Route {
@@ -6,7 +7,14 @@ class ApiRoute implements Route {
     /// Base RoutePrefix
     Router.basePrefix('api');
 
-    Router.get("/greet", () {
+    // Vendors Routes
+    Router.get("/vendors", VendorsController().index);
+    Router.post("/vendors", VendorsController().store);
+    Router.get("/vendors/{id}", VendorsController().show);
+    Router.put("/vendors/{id}", VendorsController().update);
+    Router.delete("/vendors/{id}", VendorsController().destroy);
+
+    Router.get("/ping", () {
       return Response.json({"message": "Hello Vania"});
     });
   }
