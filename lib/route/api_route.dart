50e3ffa6d7c0f8cef7e8ab6a76eaa2f50aebf7d7
Prawirdani <prawirdani@gmail.com>
2024-12-12 21:35:02 +0700

    group routes

--- lib/route/api_route.dart
+++ lib/route/api_route.dart
@@ -11,48 +11,68 @@ class ApiRoute implements Route {
     Router.basePrefix('api');
 
     // Vendors Routes
-    Router.get("/vendors", VendorsController().index);
-    Router.post("/vendors", VendorsController().store);
-    Router.get("/vendors/{id}", VendorsController().show);
-    Router.put("/vendors/{id}", VendorsController().update);
-    Router.delete("/vendors/{id}", VendorsController().destroy);
+    Router.group(
+      () {
+        Router.get("/", VendorsController().index);
+        Router.post("/", VendorsController().store);
+        Router.get("/{id}", VendorsController().show);
+        Router.put("/{id}", VendorsController().update);
+        Router.delete("/{id}", VendorsController().destroy);
+      },
+      prefix: "/vendors",
+    );
 
     // Products Routes
-    Router.get("/products", ProductsController().index);
-    Router.post("/products", ProductsController().store);
-    Router.get("/products/{id}", ProductsController().show);
-    Router.put("/products/{id}", ProductsController().update);
-    Router.delete("/products/{id}", ProductsController().destroy);
-    Router.post("/products/{productId}/notes", ProductsController().storeNote);
-    Router.put(
-      "/products/{productId}/notes/{noteId}",
-      ProductsController().updateNote,
-    );
-    Router.delete(
-      "/products/{productId}/notes/{noteId}",
-      ProductsController().deleteNote,
+    Router.group(
+      () {
+        Router.get("/", ProductsController().index);
+        Router.post("/", ProductsController().store);
+        Router.get("/{id}", ProductsController().show);
+        Router.put("/{id}", ProductsController().update);
+        Router.delete("/{id}", ProductsController().destroy);
+        Router.post("/{productId}/notes", ProductsController().storeNote);
+        Router.put(
+          "/{productId}/notes/{noteId}",
+          ProductsController().updateNote,
+        );
+        Router.delete(
+          "/{productId}/notes/{noteId}",
+          ProductsController().deleteNote,
+        );
+      },
+      prefix: "/products",
     );
 
     // Customers Routes
-    Router.get("/customers", CustomersController().index);
-    Router.post("/customers", CustomersController().store);
-    Router.get("/customers/{id}", CustomersController().show);
-    Router.put("/customers/{id}", CustomersController().update);
-    Router.delete("/customers/{id}", CustomersController().destroy);
+    Router.group(
+      () {
+        Router.get("/", CustomersController().index);
+        Router.post("/", CustomersController().store);
+        Router.get("/{id}", CustomersController().show);
+        Router.put("/{id}", CustomersController().update);
+        Router.delete("/{id}", CustomersController().destroy);
+      },
+      prefix: "/customers",
+    );
 
     // Orders Routes
-    Router.get("/orders", OrdersController().index);
-    Router.post("/orders", OrdersController().store);
-    Router.get("/orders/{orderNum}", OrdersController().show);
-    Router.delete("/orders/{orderNum}", OrdersController().destroy);
-    Router.post("/orders/{orderNum}/items", OrdersController().storeItem);
-    Router.put(
-      "/orders/{orderNum}/items/{itemId}",
-      OrdersController().updateItem,
-    );
-    Router.delete(
-      "/orders/{orderNum}/items/{itemId}",
-      OrdersController().destroyItem,
+    Router.group(
+      () {
+        Router.get("/", OrdersController().index);
+        Router.post("/", OrdersController().store);
+        Router.get("/{orderNum}", OrdersController().show);
+        Router.delete("/{orderNum}", OrdersController().destroy);
+        Router.post("/{orderNum}/items", OrdersController().storeItem);
+        Router.put(
+          "/{orderNum}/items/{itemId}",
+          OrdersController().updateItem,
+        );
+        Router.delete(
+          "/{orderNum}/items/{itemId}",
+          OrdersController().destroyItem,
+        );
+      },
+      prefix: "/orders",
     );
 
     Router.get("/ping", () {
